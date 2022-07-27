d6fbf38c64b025161e4c256b26b3fb298e1bddad
naitikraj1000 <95161534+naitikraj1000@users.noreply.github.com>
2022-07-27 12:12:36 +0530

    Add files via upload
    Calculator Subs Program For substraction purpose

---
+++ calculator_Subs.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+// Testing cal for Addition Program
+int main(){
+      printf("Enter Two Elements \n");
+    float  n1,n2;
+    scanf("%f",&n1); scanf("%f",&n2);
+    printf("Result--> (%f-%f) = %f",n1,n2,n1-n2);
+}
